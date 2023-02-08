f53167e6a76d7fb7eadf180bf955eed536e7f950
hebingxu <1715924014>
2023-02-08 17:28:54 +0800

    缺少impl

---
+++ src/main/java/com/bigdata/backstage/modules/common/service/impl/MetDataOverviewLabelRelationServiceImpl.java
@@ -0,0 +1,20 @@
+package com.bigdata.backstage.modules.common.service.impl;
+
+import com.bigdata.backstage.modules.common.model.MetDataOverviewLabelRelation;
+import com.bigdata.backstage.modules.common.mapper.MetDataOverviewLabelRelationMapper;
+import com.bigdata.backstage.modules.common.service.MetDataOverviewLabelRelationService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 数据资产标签映射表 服务实现类
+ * </p>
+ *
+ * @author bigdata
+ * @since 2023-02-08
+ */
+@Service
+public class MetDataOverviewLabelRelationServiceImpl extends ServiceImpl<MetDataOverviewLabelRelationMapper, MetDataOverviewLabelRelation> implements MetDataOverviewLabelRelationService {
+
+}
---
+++ src/main/java/com/bigdata/backstage/modules/common/service/impl/MetDwInfoServiceImpl.java
@@ -0,0 +1,20 @@
+package com.bigdata.backstage.modules.common.service.impl;
+
+import com.bigdata.backstage.modules.common.model.MetDwInfo;
+import com.bigdata.backstage.modules.common.mapper.MetDwInfoMapper;
+import com.bigdata.backstage.modules.common.service.MetDwInfoService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 数仓主表 服务实现类
+ * </p>
+ *
+ * @author macro
+ * @since 2023-02-08
+ */
+@Service
+public class MetDwInfoServiceImpl extends ServiceImpl<MetDwInfoMapper, MetDwInfo> implements MetDwInfoService {
+
+}
